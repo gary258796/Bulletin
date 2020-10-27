1a10a0c20ff181b96bdd5445baf2ad28ad475944
Gary <gary258796@gmail.com>
2020-10-27 10:04:35 +0800

    chore: delete no need settings

--- src/main/java/gary/springframework/bulletin/configs/WebConfig.java
+++ src/main/java/gary/springframework/bulletin/configs/WebConfig.java
@@ -9,7 +9,6 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -71,20 +70,6 @@ public class WebConfig implements WebMvcConfigurer {
         LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
         validator.setValidationMessageSource(messageSource());
         return validator;
-    }
-
-
-    /**
-     * 用到 addResourceHandler 的內容
-     * 會自動被替換成 addResourceLocations 的內容
-     * @param registry
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/")
-                .resourceChain(false);
     }
 
     /**
