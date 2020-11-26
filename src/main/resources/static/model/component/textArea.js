b8ca279f44f38c2308dbfd15a8db977512c19a0f
Gary <gary258796@gmail.com>
2020-11-26 14:45:06 +0800

    chore : Add Text-Area component

---
+++ src/main/resources/static/model/component/textArea.js
@@ -0,0 +1,35 @@
+'use strict'
+
+Vue.component(
+    'vue-text-area',
+    baseVue.extend({
+        template: '#vue-text-area-template',
+        mounted: function (){
+            const self = this;
+            let autoExpandElement = document.getElementsByClassName("autoExpand");
+            for(let i = 0 ; i < autoExpandElement.length; i++){
+                autoExpandElement[i].addEventListener('input', self.onExpandableTextareaInput);
+            }
+        },
+        methods: {
+            onExpandableTextareaInput : function ({ target:elm }){
+                const self = this ;
+                // make sure the input event originated from a textarea and it's desired to be auto-expandable
+                if( !elm.classList.contains('autoExpand') || !elm.nodeName == 'TEXTAREA' ) return
+
+                var minRows = elm.getAttribute('data-min-rows')|0, rows;
+                !elm._baseScrollHeight && self.getScrollHeight(elm)
+
+                elm.rows = minRows
+                rows = Math.ceil((elm.scrollHeight - elm._baseScrollHeight) / 16)
+                elm.rows = minRows + rows
+            },
+            getScrollHeight : function (elm){
+                var savedValue = elm.value
+                elm.value = ''
+                elm._baseScrollHeight = elm.scrollHeight
+                elm.value = savedValue
+            }
+        }
+    })
+);
