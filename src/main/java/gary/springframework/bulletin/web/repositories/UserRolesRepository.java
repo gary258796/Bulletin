d7ab242546cfa7e5832e23d7e9906587ad45c176
Gary <gary258796@gmail.com>
2021-04-29 17:44:32 +0800

    chore: Add UserRoles Entity and repository

---
+++ src/main/java/gary/springframework/bulletin/web/repositories/UserRolesRepository.java
@@ -0,0 +1,15 @@
+package gary.springframework.bulletin.web.repositories;
+
+import gary.springframework.bulletin.data.entity.Role;
+import gary.springframework.bulletin.data.entity.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface UserRolesRepository {
+
+
+    void storeUserRoles(User user, Collection<Role> roles);
+
+}
