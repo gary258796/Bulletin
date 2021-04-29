f3f6e8fcad2331990c0f2d566541f0add2aad8fb
Gary <gary258796@gmail.com>
2021-04-29 18:26:55 +0800

    chore: Add method of UserRolesRepository

--- src/main/java/gary/springframework/bulletin/web/repositories/UserRolesRepository.java
+++ src/main/java/gary/springframework/bulletin/web/repositories/UserRolesRepository.java
@@ -3,13 +3,13 @@ package gary.springframework.bulletin.web.repositories;
 import gary.springframework.bulletin.data.entity.Role;
 import gary.springframework.bulletin.data.entity.User;
 import org.springframework.stereotype.Repository;
-
 import java.util.Collection;
 
 @Repository
 public interface UserRolesRepository {
 
-
     void storeUserRoles(User user, Collection<Role> roles);
+
+    Collection<Role> getRolesByUserId(int userId);
 
 }
