f4d9c42f2c84dbb860225bd282daa859f4e8bc05
Gary <gary258796@gmail.com>
2021-04-29 21:13:17 +0800

    Refactor: UserService and userRepository

--- src/main/java/gary/springframework/bulletin/web/repositories/UserRepository.java
+++ src/main/java/gary/springframework/bulletin/web/repositories/UserRepository.java
@@ -3,7 +3,7 @@ package gary.springframework.bulletin.web.repositories;
 import gary.springframework.bulletin.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUserName(String userName);
 
