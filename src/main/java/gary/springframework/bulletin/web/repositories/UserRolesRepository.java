d67f1696cf3311ff802d04a426072bd161b95971
Gary <gary258796@gmail.com>
2021-05-03 10:50:47 +0800

    Refactor: User_Roles entity and repository

--- src/main/java/gary/springframework/bulletin/web/repositories/UserRolesRepository.java
+++ src/main/java/gary/springframework/bulletin/web/repositories/UserRolesRepository.java
@@ -2,9 +2,12 @@ package gary.springframework.bulletin.web.repositories;
 
 import gary.springframework.bulletin.data.entity.Role;
 import gary.springframework.bulletin.data.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Collection;
 
+@Mapper
 @Repository
 public interface UserRolesRepository {
 
@@ -13,13 +16,14 @@ public interface UserRolesRepository {
      * @param user :
      * @param roles : roles user owned
      */
-    void storeUserRoles(User user, Collection<Role> roles);
+    void storeUserRoles(@Param("user") User user,
+                        @Param("roles") Collection<Role> roles);
 
     /**
      * Get roles of user with userId == @id
      * @param userId:
      * @return Role of user , whose id == @userId
      */
-    Collection<Role> getRolesByUserId(int userId);
+    Collection<Role> getRolesByUserId(@Param("userId") int userId);
 
 }
