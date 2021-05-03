f01f2c77fd0c26fe31616e5b54292d98342b2361
Gary <gary258796@gmail.com>
2021-05-03 10:39:33 +0800

    chore: Add missing @Repository annotation

--- src/main/java/gary/springframework/bulletin/web/repositories/UserRepository.java
+++ src/main/java/gary/springframework/bulletin/web/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package gary.springframework.bulletin.web.repositories;
 
 import gary.springframework.bulletin.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUserName(String userName);
--- src/main/java/gary/springframework/bulletin/web/repositories/VerificationTokenRepository.java
+++ src/main/java/gary/springframework/bulletin/web/repositories/VerificationTokenRepository.java
@@ -2,7 +2,9 @@ package gary.springframework.bulletin.web.repositories;
 
 import gary.springframework.bulletin.data.entity.token.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 
     VerificationToken findByToken(String token);
